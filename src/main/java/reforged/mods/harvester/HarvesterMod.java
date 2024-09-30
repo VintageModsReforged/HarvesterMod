f3ab9ccccdb6882d6ef01b975fb7a891cc7e36a2
cross <cross.wanted.1.4@gmail.com>
2024-09-30 17:20:49 +0900

    version index update

--- src/main/java/reforged/mods/harvester/HarvesterMod.java
+++ src/main/java/reforged/mods/harvester/HarvesterMod.java
@@ -3,7 +3,7 @@ package reforged.mods.harvester;
 import cpw.mods.fml.common.Mod;
 import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid = "harvester", name = "Harvester Mod", version = "1.5.2-1.0.0")
+@Mod(modid = "harvester", name = "Harvester Mod", version = "1.5.2-1.0.1")
 public class HarvesterMod {
 
     public HarvesterMod() {
