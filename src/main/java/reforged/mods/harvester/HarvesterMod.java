088e4b4edf8c496abd8c61d7283e6f8ba3c3e2e3
cross <cross.wanted.1.4@gmail.com>
2024-10-03 15:44:22 +0900

    fixed NPE with heldStack

--- src/main/java/reforged/mods/harvester/HarvesterMod.java
+++ src/main/java/reforged/mods/harvester/HarvesterMod.java
@@ -7,7 +7,7 @@ import net.minecraftforge.common.MinecraftForge;
 
 import java.util.logging.Logger;
 
-@Mod(modid = "harvester", name = "Harvester Mod", version = "1.4.7-1.0.4")
+@Mod(modid = "harvester", name = "Harvester Mod", version = "1.4.7-1.0.4.1")
 public class HarvesterMod {
 
     public static final Logger LOGGER = Logger.getLogger("Harvester Mod");
