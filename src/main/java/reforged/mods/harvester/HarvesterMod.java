0b25d3ba8b904aa619b217a77224966882f8db72
CrossVas <cross.wanted.1.4@gmail.com>
2025-06-03 23:05:09 +0900

    change: Improve leaf decay ASM transformer

--- src/main/java/reforged/mods/harvester/HarvesterMod.java
+++ src/main/java/reforged/mods/harvester/HarvesterMod.java
@@ -15,7 +15,7 @@ import reforged.mods.harvester.events.GrowthEvent;
 
 import java.util.logging.Logger;
 
-@Mod(modid = "harvester", name = "Harvester Mod", version = "1.5.2-1.0.5.6")
+@Mod(modid = "harvester", name = "Harvester Mod", useMetadata = true)
 public class HarvesterMod {
 
     public static final Logger LOGGER = Logger.getLogger("Harvester Mod");
--- src/main/java/reforged/mods/harvester/asm/LeafDecayTransformer.java
+++ src/main/java/reforged/mods/harvester/asm/LeafDecayTransformer.java
@@ -17,6 +17,7 @@ public class LeafDecayTransformer implements IClassTransformer {
         this.obfStrings = new HashMap<String, String>();
         this.obfStrings.put("blockClassName", "apa"); // net.minecraft.block.Block
         this.obfStrings.put("methodName", "a"); // onNeighborBlockChange
+        this.obfStrings.put("methodDesc", "(Laab;IIII)V");
     }
 
     @Override
@@ -26,30 +27,36 @@ public class LeafDecayTransformer implements IClassTransformer {
 
     private byte[] transform(byte[] basicClass, Map<String, String> names) {
         HarvesterMod.LOGGER.info("[Leaf Decay - ASM] Transforming Classes!");
-        HarvesterMod.LOGGER.info("[Leaf Decay - ASM] Class Transformation running on " + names.get("blockClassName") + "...");
+        HarvesterMod.LOGGER.info("[Leaf Decay - ASM] Class transformation running on " + names.get("blockClassName") + "...");
+
         ClassNode classNode = new ClassNode();
         ClassReader classReader = new ClassReader(basicClass);
         classReader.accept(classNode, 0);
+
         List<MethodNode> methods = classNode.methods;
         for (MethodNode methodNode : methods) {
-            if (methodNode.name.equals(names.get("methodName")) && methodNode.desc.equals("(Laab;IIII)V")) {
-                HarvesterMod.LOGGER.info("[Leaf Decay - ASM] Found target method " + methodNode.name + methodNode.desc + "! Searching for landmarks...");
-                HarvesterMod.LOGGER.info("[Leaf Decay - ASM] Patching method " + names.get("blockClassName") + "/" + methodNode.name + methodNode.desc + "...");
-                LabelNode lmm1Node = new LabelNode(new Label());
+            if (methodNode.name.equals(names.get("methodName")) && methodNode.desc.equals(names.get("methodDesc"))) {
+                HarvesterMod.LOGGER.info("[Leaf Decay - ASM] Found target method " + methodNode.name + methodNode.desc + "! Patching...");
+
                 InsnList toInject = new InsnList();
                 toInject.add(new VarInsnNode(Opcodes.ALOAD, 1));
                 toInject.add(new VarInsnNode(Opcodes.ILOAD, 2));
                 toInject.add(new VarInsnNode(Opcodes.ILOAD, 3));
                 toInject.add(new VarInsnNode(Opcodes.ILOAD, 4));
                 toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "reforged/mods/harvester/events/LeafDecayEvent", "onLeafDecay", "(Laab;III)V"));
-                toInject.add(lmm1Node);
-                methodNode.instructions.insert(toInject);
+
+                AbstractInsnNode insertPoint = methodNode.instructions.getFirst();
+                while (insertPoint instanceof LabelNode || insertPoint instanceof LineNumberNode) {
+                    insertPoint = insertPoint.getNext();
+                }
+                methodNode.instructions.insertBefore(insertPoint, toInject);
+
                 HarvesterMod.LOGGER.info("[Leaf Decay - ASM] Method " + names.get("blockClassName") + "/" + methodNode.name + methodNode.desc + " patched!");
                 HarvesterMod.LOGGER.info("[Leaf Decay - ASM] Patching Complete!");
                 break;
             }
         }
-        ClassWriter writer = new ClassWriter(1);
+        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
         classNode.accept(writer);
         return writer.toByteArray();
     }
